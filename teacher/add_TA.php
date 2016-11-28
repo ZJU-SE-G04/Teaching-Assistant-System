af1c5651a491d19e0572f11312cfaa1f4544424a
OldWa <xiaowayy@outlook.com>
2016-11-28 15:34:48 +0800

    update add_TA.php

--- teacher/add_TA.php
+++ teacher/add_TA.php
@@ -3,18 +3,18 @@
 	include 'connect.php';
 	$class_id=$_GET["class_id"];//获取教师账号
 	//$class_id=1;
-	
+	if($class_id==null) echo 'SHIT';
 	$result=$conn->query('select * from class_table where class_id='.$class_id);
 	$row=mysqli_fetch_assoc($result);
 	$lesson_id=$row['lesson_id'];
 
 	$su=1;
 	$erm='null';
-	for ($i=0;$i<sizeof($_POST['id']);$i++){
-		$id=$_POST['id'][$i];
-		$name=$_POST['name'][$i];
-		$dep=$_POST['department'][$i];
-		$maj=$_POST['major'][$i];
+	for ($i=0;$i<sizeof($_GET['id']);$i++){
+		$id=$_GET['id'][$i];
+		$name=$_GET['name'][$i];
+		$dep=$_GET['department'][$i];
+		$maj=$_GET['major'][$i];
 		//如果没有这个账号就先在USER表中增加该账号
 		$result=$conn->query('insert into user_table values("'.$id.'","'.$id.'","'.$mame.'",2,null,null,null);');
 		$result=$conn->query('insert into assitant_table values("'.$id.'","'.$mame.'","'.$dep.'","'.$maj.'");');
@@ -26,7 +26,7 @@
 	}
 
 
-	$arr['success']=$su;
+	$arr['if_success']=$su;
 	$arr['error_message']=$erm;
 	echo json_encode($arr, JSON_UNESCAPED_UNICODE);
 	$conn->close();
