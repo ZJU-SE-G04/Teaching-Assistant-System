07a835a1e90afb7aa6ed4076d5e06c5e057153f7
OldWa <xiaowayy@outlook.com>
2016-12-01 13:43:44 +0800

    发布文章后端

---
+++ teacher/add_article.php
@@ -0,0 +1,44 @@
+<?php
+	//尚未测试
+	include 'connect.php';
+	$lesson_id=$_GET["lesson_id"];//获取班级号
+	//$lesson_id='ABCDE1';
+
+	$su=1;
+	$erm='null';
+
+	for($i=0;$i<1;$i++){
+		$id=$_GET['id'];
+		if($id==null){
+			$su=0;
+			$erm='发帖人ID不能为空';
+			break;
+		}
+		$title=$_GET['title'];
+		if($title==null){
+			$su=0;
+			$erm='请输入文章标题';
+			break;
+		}
+		$content=$_GET['content'];
+		if($content==null){
+			$su=0;
+			$erm='文章内容不能为空';
+			break;
+		}
+		$datetime=$_GET['datetime'];
+
+		$result=$conn->query('insert into article_table values(null,"'.$lesson_id.'","'.$id.'","'.$title.'","'.$content.'","'.$datetime.'");');
+		
+		if($result==null){
+			$su=0;
+			$erm='发布失败，请检查网络连接后重试';
+		}
+	}
+
+	$arr['if_success']=$su;
+	$arr['error_message']=$erm;
+	echo json_encode($arr, JSON_UNESCAPED_UNICODE);
+	$conn->close();
+
+?>
