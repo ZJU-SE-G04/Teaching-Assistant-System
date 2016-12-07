2afd02f862434a13bb5290c74fba3d5211e58d57
OldWa <xiaowayy@outlook.com>
2016-12-07 16:45:08 +0800

    修改显示文章后端，新增更新文章后端

---
+++ teacher/update_article.php
@@ -0,0 +1,50 @@
+<?php
+	//尚未测试
+	include 'connect.php';
+	$lesson_id=$_GET["lesson_id"];//获取班级号
+	//$lesson_id='ABCDE1';
+
+	$su=1;
+	$erm='null';
+
+	for($i=0;$i<1;$i++){
+		$old_id=(int)$_GET['article_id'];
+		//$old_id=9;
+
+		$id=$_GET['id'];
+		//$id='111111';
+		if($id==null){
+			$su=0;
+			$erm='发帖人ID不能为空';
+			break;
+		}
+		$title=$_GET['title'];
+		//$title='测试时间';
+		if($title==null){
+			$su=0;
+			$erm='请输入文章标题';
+			break;
+		}
+		$content=$_GET['content'];
+		//$content='时间应该是对的';
+		if($content==null){
+			$su=0;
+			$erm='文章内容不能为空';
+			break;
+		}
+		$datetime=date('Y-m-d H:i:s');
+
+		$result=$conn->query('update article_table set lesson_id="'.$lesson_id.'",id="'.$id.'",title="'.$title.'",content="'.$content.'",time="'.$datetime.'" where article_id='.$old_id.';');
+		
+		if($result==null){
+			$su=0;
+			$erm='发布失败，请检查网络连接后重试';
+		}
+	}
+
+	$arr['if_success']=$su;
+	$arr['error_message']=$erm;
+	echo json_encode($arr, JSON_UNESCAPED_UNICODE);
+	$conn->close();
+
+?>
