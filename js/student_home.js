f2dcf3386a041d0ae4c0f0e5a865d689ade92cee
singleghost <570111267@qq.com>
2016-12-09 11:07:36 +0800

    完善学生个人主页的处理入队申请和通知功能

---
+++ js/student_home.js
@@ -0,0 +1,11 @@
+/**
+ * Created by dddong on 16/12/9.
+ */
+
+$(document).ready(
+//        $("#mod_pass_submit_btn").click(function () {
+//        })
+    $(".alert").bind("closed.bs.alert", function () {
+        alert("警告框被关闭");
+    })
+)
