62e44092b7efcd0ed8cec7ff0b7c6aa0adb187d0
OldWa <xiaowayy@outlook.com>
2016-12-13 00:15:51 +0800

    现在还没写好SESSION，所以这部分数据是写死的，都能显示一部分

---
+++ student/modify_pass.php
@@ -0,0 +1,37 @@
+<?php
+	include 'connect.php';
+	//session_start();
+	//$uid=$_SESSION['user'];
+	//$oldpass=$_GET['oldpass'];
+	//$newpass=$_GET['newpass'];
+	$uid='3140101111';
+	$oldpass='1234';
+	$newpass='1111';
+
+	$state=0;
+	$msg='';
+
+	$result = $conn->query('select * from user_table where id="'.$uid.'";');
+	$row=mysqli_fetch_assoc($result);
+	$realpass=$row['password'];
+	
+	if($oldpass==$realpass){
+		$result=$conn->query('update user_table set password="'.$newpass.'" where id="'.$uid.'";');
+		if($result){
+			$state=0;
+			$msg='修改成功';
+		}else{
+			$state=2;
+			$msg='修改失败！请检查新密码是否格式正确！';
+		}
+	}else{
+		$state=1;
+		$msg='修改失败！请检查密码是否正确！';
+	}
+	
+	$arr['state']=$state;
+	$arr['msg']=$msg;
+	echo json_encode($arr, JSON_UNESCAPED_UNICODE);
+	$conn->close();
+
+?>
