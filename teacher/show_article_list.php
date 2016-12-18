1557f5777a593cb0a415252308b6484f26940202
OldWa <xiaowayy@outlook.com>
2016-12-18 15:12:34 +0800

    update show_article_list.php

--- teacher/show_article_list.php
+++ teacher/show_article_list.php
@@ -1,9 +1,10 @@
 <?php
 	include 'connect.php';
+	error_reporting(E_ERROR|E_WARNING);
 	$lesson_id=$_GET["lesson_id"];//获取教师账号
 	$search=$_GET['needed_title'];
 	//$lesson_id='ABCDE1';
-	//$search='xx';
+	//$search='';
 
 	$result = $conn->query("select * from article_table natural join teacher_table where lesson_id='".$lesson_id."' and title like '%".$search."%' order by article_id DESC;");
 
@@ -13,7 +14,7 @@
 		$x['title']=$row['title'];
 		$x['id']=$row['id'];
 		$x['user_name']=$row['name'];
-		$x['short_content']=substr($row['content'],0,400)."...";
+		$x['short_content']=mb_substr(htmlspecialchars_decode($row['content']),0,60,'utf-8')."...";
 		$x['time']=$row['time'];
 		$arr[] = $x;
 	}
