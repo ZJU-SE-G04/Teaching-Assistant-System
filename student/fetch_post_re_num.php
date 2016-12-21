75860f81a48c8ff6e9f9f1c72bf0d92c9276cf33
OldWa <xiaowayy@outlook.com>
2016-12-21 18:57:46 +0800

    计算帖子评论数

---
+++ student/fetch_post_re_num.php
@@ -0,0 +1,15 @@
+<?php
+	include 'connect.php';
+	$topic_id=$_GET["topic_id"];//获取教师账号
+	//$topic_id=1;
+	//$lesson_id='ABCDE1';
+	//$post_kind=1;
+
+	$result = $conn->query("select count(*) from response_table where topic_id=".$topic_id.";");
+	$row=mysqli_fetch_assoc($result);
+	$arr['num']=$row['count(*)'];
+	
+	echo json_encode($arr, JSON_UNESCAPED_UNICODE);
+	$conn->close();
+
+?>
