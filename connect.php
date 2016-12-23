5695235484c5ebc4835a284966fba90dfafc3e07
OldWa <xiaowayy@outlook.com>
2016-12-23 10:28:37 +0800

    update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
-	header("Access-Control-Allow-Origin: *");
-	header("Content-Type: application/json; charset=UTF-8");
+	//header("Access-Control-Allow-Origin: *");
+	//header("Content-Type: application/json; charset=UTF-8");
 	$servername = "120.77.34.254";
 	$username = "root";
 	$password = "exciting";
