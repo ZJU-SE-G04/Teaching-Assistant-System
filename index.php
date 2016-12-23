b56acbfb42015988d5c6446ca5df9f55842e266a
OldWa <xiaowayy@outlook.com>
2016-12-23 11:02:41 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,13 +1,42 @@
 <?php
 	session_start();
 	$action=$_POST['action'];
-	//$action='login';
+	//$action='jump_to_lesson';
 	if($action=='isLogin'){
 		isLogin();
 	}else if($action=='login'){
 		login();
 	}else if($action=='logout'){
 		logout();
+	}else if($action=='jump_to_lesson'){
+		jumpto();
+	}
+
+	function jumpto(){
+		include 'connect.php';
+		$uid=$_SESSION['user'];
+		//$uid='111111';
+		$lesson_id=$_POST['lesson_id'];
+		//$lesson_id='ABCDE1';
+		$result=$conn->query('select * from user_table where id="'.$uid.'"');
+		$row=mysqli_fetch_assoc($result);
+		if($row!=null){
+			$uname=$row['user_name'];
+			switch($row['level']){
+				case 1:{
+				//echo 'Location:/student/student_learn.html?lesson_id='.$lesson_id;
+				header('Location:/student/student_learn.html?lesson_id='.$lesson_id);
+				break;
+			}
+				case 3:{
+					$result=$conn->query('select * from lesson_table where lesson_id="'.$lesson_id.'"');
+					$row=mysqli_fetch_assoc($result);
+					//echo 'Location:teacher/show_article.html?lesson_id='.$lesson_id.'&course_name='.$row['lesson_name'].'&user_name='.$uname.'&user_id='.$uid;
+					header('Location:teacher/show_article.html?lesson_id='.$lesson_id.'&course_name='.$row['lesson_name'].'&user_name='.$uname.'&user_id='.$uid);
+					break;
+				}
+			}
+		}
 	}
 
 	function isLogin(){
