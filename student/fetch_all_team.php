db83f3a42349eae81c5f49c9e4fc97b2085b6112
OldWa <xiaowayy@outlook.com>
2016-12-23 17:15:02 +0800

    获取所有队伍

---
+++ student/fetch_all_team.php
@@ -0,0 +1,19 @@
+<?php
+	include 'connect.php';
+
+	$result = $conn->query("select * from team_table;");
+	
+	$arr = [];
+
+	while($row = mysqli_fetch_assoc($result)) {
+		$x['teamID']=(int)$row['team_id'];
+		$x['teamName']=$row['team_name'];
+		$x['max']=$row['max'];
+		$x['joined']=$row['now'];
+		$arr[]=$x;
+	}
+	
+	echo json_encode($arr, JSON_UNESCAPED_UNICODE);
+	$conn->close();
+
+?>
