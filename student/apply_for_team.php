45627cb4caeb2c3b928127d378bfdd438739138c
OldWa <xiaowayy@outlook.com>
2016-12-24 12:34:00 +0800

    update apply_for_team.php

--- student/apply_for_team.php
+++ student/apply_for_team.php
@@ -4,7 +4,7 @@
 	$uid=$_SESSION['user'];
 	$teamID=(int)$_GET['teamID'];
 	//$uid='3140106666';
-	//$teamID=5;
+	//$teamID=2;
 	
 	$state=0;
 	$msg='';
@@ -16,9 +16,16 @@
 		$state=1;
 		$msg='你已经加入队伍了！';
 	}else{//添加入队请求
-		$result=$conn->query('insert into orgnize_table values("'.$uid.'",'.$teamID.',0);');
-		$state=0;
-		$msg='入队申请已发送！等待队伍成员同意...';
+		$result=$conn->query('select * from team_table where team_id='.$teamID.';');
+		$row=mysqli_fetch_assoc($result);
+		if($row['now']<$row['max']){
+			$result=$conn->query('insert into orgnize_table values("'.$uid.'",'.$teamID.',0);');
+			$state=0;
+			$msg='入队申请已发送！等待队伍成员同意...';
+		}else{
+			$state=1;
+			$msg='人数已满！';
+		}
 	}
 
 	$arr['state']=$state;
