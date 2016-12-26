409fbf0bbed9ee079c4244f872ce25c65d9496ff
OldWa <xiaowayy@outlook.com>
2016-12-26 13:30:46 +0800

    更新包含

---
+++ connect1.php
@@ -0,0 +1,10 @@
+<?php
+	$servername = "120.77.34.254";
+	$username = "root";
+	$password = "exciting";
+	$dbname='teaching_db';
+	date_default_timezone_set('prc');
+	// 创建连接
+	$conn = new mysqli($servername, $username, $password,$dbname);
+	$conn->query("set names utf8;");
+?>
