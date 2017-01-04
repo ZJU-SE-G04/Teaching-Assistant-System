1efaee2c336c9307039b1833ebb1f9b5100187aa
achao <zhangshichao628@sina.com>
2017-01-04 10:37:35 +0800

    修复'添加老师教师表不能插入'的bug

--- administrator/addTeacher.php
+++ administrator/addTeacher.php
@@ -5,7 +5,7 @@
 	$teacherName=$teacherName.'老师';
 	$teacherIntroduction=$_POST["teacherIntroduction"];
 	$pwd=md5($teacherId);
-    $result = $conn->query('insert into teacher_table values("'.$teacherId.'","'.$teacherName.'","'.$teacherIntroduction.'");');
 	$result = $conn->query('insert into user_table values("'.$teacherId.'","'.$pwd.'","'.$teacherName.'",3,NULL,NULL,NULL);');
+    $result = $conn->query('insert into teacher_table values("'.$teacherId.'","'.$teacherName.'","'.$teacherIntroduction.'");');
 	$conn->close();
 ?>
